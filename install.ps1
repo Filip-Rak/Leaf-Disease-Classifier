454d13b1818f4bf11bbaaabf2d0faede16ae757f
Filip-Rak <73279456+Filip-Rak@users.noreply.github.com>
2025-03-20 23:08:54 +0100

    change directory structure. Add models based on custom EfficientNet

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 python -m venv .venv
 .\.venv\Scripts\Activate
 pip install torch torchvision --index-url https://download.pytorch.org/whl/cu126
-pip install matplotlib scikit-learn seaborn
+pip install matplotlib scikit-learn seaborn kornia
